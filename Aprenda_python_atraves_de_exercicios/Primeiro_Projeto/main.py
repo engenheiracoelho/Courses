3fbcc6848e15e086cbb4e248e79af6aaad5bea19
Leticia <leticia.ac23@gmail.com>
2018-05-30 14:18:48 -0300

    Figura Operadores Booleanos

--- Aprenda_python_atraves_de_exercicios/Primeiro_Projeto/main.py
+++ Aprenda_python_atraves_de_exercicios/Primeiro_Projeto/main.py
@@ -2,7 +2,7 @@
 print("Hello Neps Academy!")
 
 #Aula2
-
+'''
 v=10
 a=True
 b=False
@@ -11,10 +11,10 @@ print(v)
 print(a)
 print(b)
 print(c)
-
+'''
 
 #Aula3
-
+'''
 A=4
 B=3
 print("O valor da soma é {} e o valor da divisão é {}".format(A+B,A/B))
@@ -25,5 +25,33 @@ print("Resultado da divisão (parte inteira): {:.0f}".format(A/B))
 #imprimir duas linhas em uma usando o print
 print("Neps", end=" ") #end muda o ultimo caracter
 print("Academy")
+'''
 
-#Aula6
+#Divisão - Aula 7
+'''
+print("Divisão com duas casas decimais")
+print("Informe o valor de A")
+A = float(input())
+print("Informe o valor de B")
+B = float(input())
+S=A/B
+print("{:.2f}".format(S))
+'''
+
+#Comentarios - Aula 8
+
+#Estrutura de decisão - Aula 9
+'''
+print("Estruturas de decisão")
+print("Informe a nota do aluno")
+A = float(input())
+if A >= 7:
+    print("Aluno aprovado")
+elif A>= 5:
+    print("Aluno em recuperação")
+else:
+    print("Aluno reprovado")
+print("Final de semestre")
+'''
+
+
