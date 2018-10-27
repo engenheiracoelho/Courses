f4d52016c7267f6ab948878d3d86bb42c249669d
LeticiaCoelho <leticia.ac23@gmail.com>
2018-10-27 00:22:53 -0200

    Ex6 - React Course

---
+++ React/Ex6/ex/index.js
@@ -0,0 +1,8 @@
+import './style.css'
+import 'react'
+
+export default props => (
+	<h1>Olá</h1>
+)
+
+console.log('Everything ok!')
---
+++ React/Ex6/webpack.config.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+	entry: './ex/index.js',
+	output: {
+		path: __dirname + '/public',
+		filename: './bundle.js',
+	},
+	devServer: {
+		port: 8080,
+		contentBase: './public'
+	},
+	plugins: [ 
+		new ExtractTextPlugin('app.css')
+	],
+	module: {
+		loaders: [{
+			test: /.js?$/,
+			loader: 'babel-loader',
+			exclude: /node_modules/,
+			query: {
+				presets: ['es2015','react'],
+				plugins: ['transform-object-rest-spread']
+			}
+		},{	
+			test: /\.css$/,
+			loader: ExtractTextPlugin.extract("style-loader","css-loader")
+		}]
+	}
+}
