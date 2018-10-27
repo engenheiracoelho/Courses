fb8ade1ad734b287bbb346e221efc0dfd042e80c
LeticiaCoelho <leticia.ac23@gmail.com>
2018-10-27 12:39:32 -0200

    Children prop

---
+++ React+Redux/React/Ex6/ex/silvaFamily.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Member from './member'
+
+export default props => (
+	<div>
+		<Member name='João' lastName='Silva' />
+		<Member name='Maria' lastName='Silva' />
+	</div>
+)
