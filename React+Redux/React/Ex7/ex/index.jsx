9c41e0a802272d0d4b92702a173b88f0b56e6ecd
LeticiaCoelho <leticia.ac23@gmail.com>
2018-10-27 12:55:11 -0200

    Component based on class - Ex7

---
+++ React+Redux/React/Ex7/ex/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ClassComponent from './classComponent'
+
+ReactDOM.render(
+	<ClassComponent value='Class Component' />
+, document.getElementById('app'))
+
---
+++ React+Redux/React/Ex7/utils/reactUtils.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function childrenWithProps(children,props) {
+	return React.Children.map(props.children,
+		child => React.cloneElement(child, {...props}))
+}
+
+export {childrenWithProps}
