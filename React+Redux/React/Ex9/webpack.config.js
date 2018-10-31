85a88134ade927c6bdee7013695759e2abc3b574
LeticiaCoelho <leticia.ac23@gmail.com>
2018-10-31 13:40:19 -0300

    Componentes

---
+++ React+Redux/React/Ex9/webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack')
+
+module.exports = {
+	entry: './ex/index.jsx',
+	output: {
+		path: __dirname + '/public',
+		filename: './bundle.js'
+	},
+	devServer: {
+		port: 8080,
+		contentBase: './public',
+	},
+	resolve: {
+		extensions: ['','.js','.jsx']
+	},
+	module: {
+		loaders: [{
+			test:/.jsx?$/,
+			loader: 'babel-loader',
+			exclude: /node_modules/,
+			query:{
+				presets: ['es2015','react'],
+				plugins: ['transform-object-rest-spread']
+			}
+		}]
+	}
+}
